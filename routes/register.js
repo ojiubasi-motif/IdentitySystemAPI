bfaae3e4b3d60684bef97bdb1d29d1b3262b4000
Oji Ubasi <ojiinnocent@gmail.com>
2023-07-16 05:09:43 +0100

    updates added again.

--- routes/register.js
+++ routes/register.js
@@ -30,7 +30,6 @@ router.post("/signup", async (req, res) => {
     try {
       let emailAlreadyExist = await Auth.findOne({ email }, "email"); //return only the email field
       let phoneAlreadyExist = await Auth.findOne({ phone }, "phone"); //return only the phone field
-      console.log("search result==>", emailAlreadyExist, phoneAlreadyExist);
       if (
         emailAlreadyExist?.email == email ||
         phoneAlreadyExist?.phone == phone
@@ -38,17 +37,18 @@ router.post("/signup", async (req, res) => {
         res
           .status(403)
           .json("A user with this email and/or phone already exist");
+      } else {
+        const savedUser = await newUser.save();
+        const {
+          password: savedPassword,
+          createdAt,
+          updatedAt,
+          ...data
+        } = savedUser._doc;
+        res
+          .status(201)
+          .json({ data, password: !password ? initialPassword : null });
       }
-      const savedUser = await newUser.save();
-      const {
-        password: savedPassword,
-        createdAt,
-        updatedAt,
-        ...data
-      } = savedUser._doc;
-      res
-        .status(201)
-        .json({ data, password: !password ? initialPassword : null });
     } catch (err) {
       console.log(err);
     }
@@ -87,10 +87,10 @@ router.post("/register", verify, async (req, res) => {
     nok_title,
     tel,
     convicted,
-    occupation,   
+    occupation,
     weight,
     height,
-    utility_bill_type
+    utility_bill_type,
   } = req.body;
   const {
     email: verifiedEmail,
@@ -104,31 +104,30 @@ router.post("/register", verify, async (req, res) => {
   const currdate = new Date();
   const bvn = "" + Math.floor(Math.random() * 10000000 + 1);
 
-
   const newUser = new Registrant({
     nin: uuidv4(),
     bvn,
     name: {
-      first_name:verifiedFirstName ,
+      first_name: verifiedFirstName,
       last_name: verifiedLAstName,
       other_names,
     },
     sex,
     bio: {
-      birth_certificate_id:`BC${bvn}`,
-      date_of_birth:  Date.now(),
+      birth_certificate_id: `BC${bvn}`,
+      date_of_birth: Date.now(),
       weight,
       height,
     },
     mother_maiden_name,
     residential_address: {
       utility_bill_type,
-      bill_number:utility_bill_type?`UB${bvn}`:null
+      bill_number: utility_bill_type ? `UB${bvn}` : null,
     },
     permanent_address: {
       house_number: p_house_number,
       street: p_street,
-      postal_code:p_postal_code,
+      postal_code: p_postal_code,
       city: p_city,
       state: p_state,
       landmark: p_landmark,
@@ -144,15 +143,15 @@ router.post("/register", verify, async (req, res) => {
     level_of_education,
 
     marital_status,
-    marital_certificate_no: marital_status !== "SINGLE"?`MC${bvn}`:null,
+    marital_certificate_no: marital_status !== "SINGLE" ? `MC${bvn}` : null,
     mother: {
       first_name: m_first_name,
       last_name: m_last_name,
-      maiden_name:mother_maiden_name,
+      maiden_name: mother_maiden_name,
       nationality: mother_nationality,
     },
     father: {
-      first_name:f_first_name ,
+      first_name: f_first_name,
       last_name: f_last_name,
       nationality: f_nationality,
     },
@@ -165,22 +164,28 @@ router.post("/register", verify, async (req, res) => {
     contact: {
       mobile: verifiedPhone,
       tel,
-      email:verifiedEmail,
+      email: verifiedEmail,
     },
     social_status: {
       convicted,
-      case_file_Id:convicted? `CF${bvn}`:null,
+      case_file_Id: convicted ? `CF${bvn}` : null,
     },
     occupation: {
-      status:occupation,
-      employer_CAC_num: occupation ==="EMPLOYED" || occupation ==="BUSINESS"?`CAC${bvn}`:null,
-      TIN:occupation ==="SELF-EMPLOYED"?`TIN${bvn}`:null
+      status: occupation,
+      employer_CAC_num:
+        occupation === "EMPLOYED" || occupation === "BUSINESS"
+          ? `CAC${bvn}`
+          : null,
+      TIN: occupation === "SELF-EMPLOYED" ? `TIN${bvn}` : null,
     },
   });
 
   try {
-    const userHasRegistered = await Registrant.findOne({"contact.mobile":verifiedPhone},'contact.mobile');
-    if(userHasRegistered){
+    const userHasRegistered = await Registrant.findOne(
+      { "contact.mobile": verifiedPhone },
+      "contact.mobile"
+    );
+    if (userHasRegistered) {
       res.status(403).json("this user has done registration");
     }
     const user = await newUser.save();
@@ -234,9 +239,8 @@ router.post("/login", async (req, res) => {
   }
 });
 
-
 // update the user record, be it password or another data
-router.put("/:id", verify, async (req, res) => {
+router.put("/update/:id", verify, async (req, res) => {
   // only the owner of the record(loggedin user) or the admin can update the record for that user
   if (req.user.id === req.params.id || req.user.isAdmin) {
     // check if it's password that's being changed and encrypt it accordingly
@@ -249,7 +253,7 @@ router.put("/:id", verify, async (req, res) => {
     // if it's not the password being updated, then continue with the try/catch
     // check if user wants to update email
     if (req.body?.contact?.email) {
-      res.status(403).json("you cannot edit your email, contact admin");
+      res.status(403).json("you cannot edit your email, contact the admin");
     }
 
     try {
@@ -265,7 +269,7 @@ router.put("/:id", verify, async (req, res) => {
       res.status(500).json(error);
     }
   } else {
-    res.status(403).json("you can update only your account");
+    res.status(403).json("you are not authorized to modify this account");
   }
 });
 
@@ -304,7 +308,10 @@ router.get("/", verify, async (req, res) => {
     try {
       // if there's query to return new users, return only the latest 2 registered else return all
       const users = query
-        ? await Registrant.find().sort({ _id: -1 }).limit(10).select("-password")
+        ? await Registrant.find()
+            .sort({ _id: -1 })
+            .limit(10)
+            .select("-password")
         : await Registrant.find();
       res.status(200).json(users);
     } catch (error) {
